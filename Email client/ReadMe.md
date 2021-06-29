524bc952dfa64997c4ac35e43fa0034c3f394201
sandipan chatterjee <sandipan.chatterjee8@gmail.com>
2021-06-29 21:30:53 +0530

    code in-progress

---
+++ Email client/ReadMe.md
@@ -0,0 +1,5 @@
+functionalities left
+1.debounce
+2.local strogage
+3.favourites
+4.active class should be there when sorted
---
+++ Email client/script.js
@@ -0,0 +1,150 @@
+let messages = [];
+let messageBody = {};
+
+const getData = async (id) => {
+  let url;
+  if (id && id.length > 0) {
+    url = `https://api.maildrop.cc/v2/mailbox/test/${id}`;
+  } else {
+    url = `https://api.maildrop.cc/v2/mailbox/test/`;
+  }
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "X-API-KEY": "QM8VTHrLR2JloKTJMZ3N6Qa93FVsx8LapKCzEjui",
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await res.json();
+  return data;
+};
+
+const displayMessageBody = async (id) => {
+  document.getElementById("message-body").innerHTML = "loading..";
+  const data = await getData(id);
+  messageBody = { ...data };
+  console.log(messageBody);
+  document.getElementById("message-body").innerHTML = `
+    <h3>
+        <span>${messageBody.from}</span>
+        <span>${messageBody.date}</span>
+    </h3>
+    <span>
+        <span>${messageBody.subject}</span>
+        <a href="#" class='delete-elements'>Delete</a>
+    </span>
+    <iframe id="inlineFrameExample"
+        display="block"
+        width="100%"
+        height="800"
+    >
+    </iframe>
+  `;
+  var ifrm = document.getElementById("inlineFrameExample");
+  var doc =
+    ifrm.contentWindow || ifrm.contentDocument.document || ifrm.contentDocument;
+  doc.document.open();
+  doc.document.write(messageBody.html);
+  doc.document.close();
+
+  //Delete Mail
+  let delEls = document.querySelectorAll(".delete-elements");
+  let url = `https://api.maildrop.cc/v2/mailbox/test/${id}`;
+  for (let delEl of delEls) {
+    delEl.addEventListener("click", async function () {
+      const res = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          "X-API-KEY": "QM8VTHrLR2JloKTJMZ3N6Qa93FVsx8LapKCzEjui",
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      document.getElementById("inbox").innerHTML = "";
+      messages = messages.filter((el) => el.id !== id);
+      displayEl();
+    });
+  }
+};
+
+function sortAsc() {
+  document.getElementById("asc").style.display = "none";
+  document.getElementById("dsc").style.display = "inline-block";
+  messages.sort((a, b) => {
+    const nameA = a.from.toUpperCase();
+    const nameB = b.from.toUpperCase();
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0;
+  });
+  document.getElementById("inbox").innerHTML = "";
+  displayEl();
+}
+
+function sortDsc() {
+  document.getElementById("dsc").style.display = "none";
+  document.getElementById("asc").style.display = "inline-block";
+  messages.sort((a, b) => {
+    const nameA = a.from.toUpperCase();
+    const nameB = b.from.toUpperCase();
+    if (nameA > nameB) return -1;
+    if (nameA < nameB) return 1;
+    return 0;
+  });
+  document.getElementById("inbox").innerHTML = "";
+  displayEl();
+}
+
+const assignClickEvent = (e) => {
+  const inboxELs = document.querySelectorAll(".inbox-elements");
+  for (let inboxEl of inboxELs) {
+    inboxEl.addEventListener("click", function () {
+      inboxELs.forEach((el) => {
+        el.classList.remove("active");
+      });
+      inboxEl.classList.add("active");
+      displayMessageBody(this.id);
+    });
+  }
+};
+const displayEl = () => {
+  document.getElementById("totalElements").innerHTML = messages.length;
+  for (let message of messages) {
+    document.getElementById("inbox").innerHTML += `
+            <div id=${message.id} class='inbox-elements'>
+                <span>
+                    <h3>${message.from}</h3>
+                    <p>${message.date}</p>  
+                </span>          
+                <p>${message.subject}</p>
+            </div>
+        `;
+  }
+  assignClickEvent();
+};
+
+document.getElementById("input").addEventListener("keyup", function () {
+  const inboxELs = document.querySelectorAll(".inbox-elements");
+  const val = this.value.toLowerCase();
+  for (let inboxEl of inboxELs) {
+    const elVal =
+      inboxEl.firstElementChild.firstElementChild.innerHTML.toLowerCase();
+    if (elVal.includes(val)) {
+      inboxEl.style.display = "block";
+    } else {
+      inboxEl.style.display = "none";
+    }
+  }
+});
+
+const initMessage = async () => {
+  document.getElementById("inbox").innerHTML = "loading..";
+  const data = await getData();
+  document.getElementById("inbox").innerHTML = "";
+  console.log(data);
+  messages.push(...data.messages);
+  console.log("messages", messages);
+  displayEl();
+};
+
+initMessage();
