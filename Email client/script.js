d3bb2d934aeffe799b547f89974f4bab2acfe96d
sandipan chatterjee <sandipan.chatterjee8@gmail.com>
2021-07-08 12:35:04 +0530

    code updated

--- Email client/script.js
+++ Email client/script.js
@@ -1,6 +1,6 @@
 let messages = [];
 let messageBody = {};
-
+const favourites = [];
 const getData = async (id) => {
   let url;
   if (id && id.length > 0) {
@@ -106,17 +106,45 @@ const assignClickEvent = (e) => {
       displayMessageBody(this.id);
     });
   }
+
+  const favs = document.querySelectorAll(".favourites");
+  for (let fav of favs) {
+    fav.addEventListener("click", function () {
+      // console.log(this.id, this.parentElement.id);
+      if (!fav.classList.contains("activeFavourites")) {
+        fav.classList.add("activeFavourites");
+        if (this.id === this.parentElement.id) {
+          favourites.push(this.parentElement);
+        }
+      } else {
+        fav.classList.remove("activeFavourites");
+        favourites.splice(favourites.indexOf(this.parentElement), 1);
+      }
+    });
+  }
 };
+
+const displayFavourites = () => {
+  document.getElementById("inbox").innerHTML = "";
+  for (let fav of favourites) {
+    console.log(fav.length);
+    document.getElementById("favouritesDisplay").append(fav);
+  }
+};
+
 const displayEl = () => {
+  document.getElementById("favouritesDisplay").innerHTML = "";
+  favourites.length = 0;
   document.getElementById("totalElements").innerHTML = messages.length;
   for (let message of messages) {
     document.getElementById("inbox").innerHTML += `
             <div id=${message.id} class='inbox-elements'>
+                <button class='favourites' id=${message.id}></button>
                 <span>
-                    <h3>${message.from}</h3>
+                    <h3>${message.from}</h3>                   
                     <p>${message.date}</p>  
                 </span>          
-                <p>${message.subject}</p>
+                <span>${message.subject}</span>              
             </div>
         `;
   }
@@ -127,8 +155,7 @@ document.getElementById("input").addEventListener("keyup", function () {
   const inboxELs = document.querySelectorAll(".inbox-elements");
   const val = this.value.toLowerCase();
   for (let inboxEl of inboxELs) {
-    const elVal =
-      inboxEl.firstElementChild.firstElementChild.innerHTML.toLowerCase();
+    const elVal = inboxEl.children[1].firstElementChild.innerHTML.toLowerCase();
     if (elVal.includes(val)) {
       inboxEl.style.display = "block";
     } else {
